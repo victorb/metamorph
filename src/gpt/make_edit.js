1011e4987df69cff961974673e248fe96812fe8f
Victor Bjelkholm <git@victor.earth>
2023-03-22 02:31:18 +0100

    ai_change

--- src/gpt/make_edit.js
+++ src/gpt/make_edit.js
@@ -2,15 +2,19 @@ const send = require('./send');
 const files_messages = require('./files_messages');
 const prompt = require('../prompts/edit');
 const temperature_setting = require('../utils/temperature_setting');
+const analyze_unsuccessful_edits = require('./analysis');
 
-function adaptive_weight(success_rate) {
+function adaptive_weight(success_rate, unsuccessful_edits_analysis) {
   const low_bound_weight = 0.3;
   const high_bound_weight = 1.5;
   const weight = low_bound_weight + (success_rate * (high_bound_weight - low_bound_weight));
+
+  // Adjust weight based on unsuccessful_edits_analysis
+
   return weight;
 }
 
-async function make_edit(suggested_edit, success_rate) {
+async function make_edit(suggested_edit, success_rate, unsuccessful_edits) {
   const messages = [
     { role: 'system', content: prompt },
     ...files_messages(),
@@ -20,13 +24,15 @@ async function make_edit(suggested_edit, success_rate) {
   const raw_result = await send(messages, temperature_setting.get_temperature());
   const parsed_result = JSON.parse(raw_result);
 
+  const unsuccessful_edits_analysis = analyze_unsuccessful_edits(unsuccessful_edits);
+
   // Add confidence scores to the edits
   const result_with_confidence = parsed_result.map((edit) => {
     const confidence = Math.max(
       success_rate,
       Math.random()
     ); // Influencing the confidence value
-    const weight = adaptive_weight(success_rate);
+    const weight = adaptive_weight(success_rate, unsuccessful_edits_analysis);
     return { ...edit, confidence: confidence * weight };
   });
 
