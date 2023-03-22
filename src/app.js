d0111b45918f0bfa02485da7111e16417d14631c
Victor Bjelkholm <git@victor.earth>
2023-03-22 21:29:07 +0100

    ai_change

--- src/app.js
+++ src/app.js
@@ -16,8 +16,21 @@ async function main() {
   console.log(edits);
   const parsed_edits = JSON.parse(edits);
   console.log('# of edits: ' + parsed_edits.length);
+
+  const initialTime = process.hrtime.bigint();
   console.log('### Applying edits');
   apply_edits(parsed_edits);
+
+  // Execute the program and store the success status and any other relevant information
+  // ...
+
+  const finalTime = process.hrtime.bigint();
+  const executionTime = finalTime - initialTime;
+  console.log('Execution time: ' + executionTime + ' nanoseconds');
+
+  // Analyze the impact of the edits on performance and determine success status
+  // ...
+
   preserve_history(proposal, expansion, edits);
   console.log('### All done!');
 }
